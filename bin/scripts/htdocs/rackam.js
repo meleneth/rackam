d3583b13d1184c0a195477dd0c55eb3842fff012
Meleneth <meleneth@gmail.com>
2012-03-17 02:59:24 -0700

    Of course, going to need this file too.  Just some code, needs cooking

---
+++ bin/scripts/htdocs/rackam.js
@@ -0,0 +1,33 @@
+this.$OuterDiv = $('<div></div>')
+  .hide()
+  .append($('<table></table>')
+    .attr({ cellSpacing : 0 })
+    .addClass("text")
+  )
+);
+
+function Page(url, start, items_per_page, num_items) {
+  this.url = url;
+  this.start = start;
+  this.items_per_page = items_per_page;
+  this.num_items = num_items;
+}
+
+Page.prototype.get_num_pages = function () {
+  return this.num_items / this.items_per_page;
+};
+
+$.getJSON('ajax/test.json', function(data) {
+  var items = [];
+
+  $.each(data, function(key, val) {
+    items.push('<li id="' + key + '">' + val + '</li>');
+  });
+
+  $('<ul/>', {
+    'class': 'my-new-list',
+    html: items.join('')
+  }).appendTo('body');
+});
+
+
