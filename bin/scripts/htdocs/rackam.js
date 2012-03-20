024c84b428fe3b8d945fd644fcbcb95429a9626f
Meleneth <meleneth@gmail.com>
2012-03-20 03:42:57 -0700

    Some in really bad need of refactoring javascript paging going on

--- bin/scripts/htdocs/rackam.js
+++ bin/scripts/htdocs/rackam.js
@@ -6,6 +6,7 @@ this.$OuterDiv = $('<div></div>')
     .addClass("text")
   )
 );
+*/
 
 function Page(url, start, items_per_page, num_items) {
   this.url = url;
@@ -18,6 +19,35 @@ Page.prototype.get_num_pages = function () {
   return this.num_items / this.items_per_page;
 };
 
+Page.prototype.previous_page = function() {
+  this.start = this.start - this.items_per_page;
+  if(this.start < 0) {
+    this.start = 0;
+  }
+  this.load_page();
+}
+
+Page.prototype.next_page = function() {
+  this.start = this.start + this.items_per_page;
+  if(this.start > (this.num_items - this.items_per_page)) {
+    this.start = this.num_items - this.items_per_page;
+  }
+  this.load_page();
+}
+
+Page.prototype.load_page = function() {
+  $.getJSON(this.url + ";page_ipp=" + this.items_per_page + ";page_first=" + this.start, function(data) {
+    $('#newsgroup-headers').empty();
+
+    $.each(data, function(i, header) {
+      $("#newsgroup-headers")
+        .append("<li>"+header.subject+"</li>")
+        .css("text-decoration", "underline");
+    });
+  });
+}
+
+/*
 $.getJSON('ajax/test.json', function(data) {
   var items = [];
 
@@ -32,14 +62,24 @@ $.getJSON('ajax/test.json', function(data) {
 });
 */
 
+rackam_pager = null;
+
 function load_newsgroup_pager(newsgroup) {
   $("#celery").empty();
-  $("#celery").append("<div></div><div><ul id=\"newsgroup-headers\"></ul></div>")
-  $.getJSON("newsgroup_headers.cgi?ng=" + newsgroup + ";pager_ipp=30", function(data) {
-    $.each(data, function(i, header) {
-      $("#newsgroup-headers").append("<li>"+header.subject+"</li>");
-    });
-  });
+  $("#celery").append("<div id=\"newsgroup-pager\"></div><div><ul id=\"newsgroup-headers\"></ul></div>");
+
+  rackam_pager = new Page("/newsgroup_headers.cgi?ng=" + newsgroup.name, 0, 30, newsgroup.num_headers);
+
+  $('<a>&lt;-</a>').click(function() {
+    rackam_pager.previous_page();
+  }).appendTo("#newsgroup-pager");
+
+  $('<a>-&gt;</a>').click(function() {
+    rackam_pager.next_page();
+  }).appendTo("#newsgroup-pager");
+
+  rackam_pager.load_page();
+
 }
 
 function setup_initial_screen() {
@@ -51,8 +91,9 @@ function setup_initial_screen() {
       $.each(data, function(i, ng) {
         $("#newsgroups-list")
           .append("<li>" + ng.name + "</li>")
+          .css("text-decoration", "underline")
           .click(function() {
-            load_newsgroup_pager(ng.name)
+            load_newsgroup_pager(ng)
           });
       });
     });
